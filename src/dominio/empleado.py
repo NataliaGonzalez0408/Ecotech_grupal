f1e7ec7e1fd6b780d7d6bcc3fb57cee406b998ae
Natalia González Ledezma <natalia.gonzalez67@inacapmail.cl>
2026-09-15 12:48:04 -0300

    archivo modificado

--- src/dominio/empleado.py
+++ src/dominio/empleado.py
@@ -1,8 +1,20 @@
 # src/dominio/empleado.py
+from datetime import date
+
 class Empleado:
-    def __init__(self, nombre: str, correo: str):
+    def __init__(self, nombre: str, direccion: str, telefono: int, correo: str, fechainiciocontrato: date, salario: int):
         self.nombre = nombre
+        self.direccion = direccion
+        self.telefono = telefono
         self.correo = correo
+        self.fechainiciocontrato = fechainiciocontrato
+        self.salario = salario
 
     def mostrar_datos(self) -> str:
-        return f"{self.nombre} - {self.correo}"
+        return f"{self.nombre} - {self.direccion} - {self.telefono} - {self.correo} - {self.fechainiciocontrato} - {self.salario}"
+
+    def gestionar_informacion(self) -> bool:
+        return True
+    
+    def actualizar_datos(self) -> bool:
+        return self.gestionar_informacion()
