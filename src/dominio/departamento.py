4f1be05e064a5894909cf6d6cfb431d52aeaf9de
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2026-09-15 12:48:05 -0300

    subiendo cambios en la clase departamento

--- src/dominio/departamento.py
+++ src/dominio/departamento.py
@@ -1,18 +1,53 @@
 from dominio.empleado import Empleado
 class Departamento:
-    def __init__(self, nombre: str):
+    def __init__(self, idDepartamento: int , nombre: str , gerente: str):
+        self.idDepartamento = idDepartamento
         self.nombre = nombre
+        self.gerente = gerente
         self._empleados: list[Empleado] = []
 
-    def agregar_empleado(self, empleado: Empleado) -> bool:
-            if empleado in self._empleados:
-                return False
-            
-                self._empleados.append(empleado)
-                return True
-    @property
-    def empleados(self) -> tuple:
-            return tuple(self._empleados)
+    def asignar_empleado(self,empleado):
 
-    def cantidad_empleados(self) -> int:
-            return len(self._empleados)
+        self.empleados.append(empleado)
+        return True
+    
+    def reasignar_empleado(self,empleado):
+        return True
+    
+    def editar(self, nombre,gerente):
+        self.nombre = nombre
+        self.gerente = gerente
+        return True
+    
+    def eliminar(self):
+        return True
+
+
+
+
+def agregar_empleado(self, empleado: Empleado) -> bool:
+    if empleado in self._empleados:
+        return False
+
+
+    self._empleados.append(empleado)
+    return True
+   
+
+
+@property
+def empleados(self) -> tuple:
+    return tuple(self._empleados)
+
+
+def cantidad_empleados(self) -> int:
+    return len(self._empleados)
+
+
+
+
+
+
+
+
+    
